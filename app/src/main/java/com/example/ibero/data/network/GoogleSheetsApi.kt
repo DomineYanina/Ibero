d039b78232fdd478fb0cbd2efdb33d12126a5dbe
Yanina Domine <yanina.domine@FUNDACION.NET>
2025-09-08 16:00:28 -0300

    Avance

--- app/src/main/java/com/example/ibero/data/network/GoogleSheetsApi.kt
+++ app/src/main/java/com/example/ibero/data/network/GoogleSheetsApi.kt
@@ -170,7 +170,7 @@ interface GoogleSheetsApiService {
 
 // Objeto singleton para acceder al servicio de red
 object GoogleSheetsApi2 {
-    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbxsOyCG8OmGtZaakoS-ft2WSmjKH6mcVApkBThOyL-TdWpqs2yPPKDhHkOuwvGq5XQKBA/"
+    private const val BASE_URL = "https://script.google.com/macros/s/AKfycbz1Dl1Fc8zUf-OFZMeuWmVF6gTcT4EWwDWte0FLna3gTzm6ZZj0IJ3mKsDiR0cjs8JCOg/"
 
     val service: GoogleSheetsApiService by lazy {
 
